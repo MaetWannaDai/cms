ff1ac5424b4044ac74fbc139237d953869bcadf8
MaetWannaDai <hyuubk2007@gmail.com>
2026-03-14 09:36:24 +0700

    made changes to homescreen, removed Stack()s

--- lib/Homescreen.dart
+++ lib/Homescreen.dart
@@ -2,7 +2,6 @@ import 'package:cms/themes/themeprovider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -14,7 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
+      backgroundColor: Theme.of(context).colorScheme.primary,
 
       // body: ListView.separated(
       //   itemCount: 5,
@@ -24,170 +23,124 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ListView(
         scrollDirection: Axis.vertical,
         children: [
-          Stack(
+          Column(
             children: [
-              Container(
-                color: Theme.of(context).colorScheme.primary,
-                height: 470,
-                margin: EdgeInsets.only(bottom: 20),
-              ),
-              Column(
+              Stack(
+                clipBehavior:
+                    Clip.none, // so that the pfp doesn't phase out of the stack
                 children: [
-                  Stack(
-                    clipBehavior: Clip
-                        .none, // so that the pfp doesn't phase out of the stack
-                    children: [
-                      buildCoverImage(),
-                      Positioned(
-                        top: 170,
-                        left: 50,
-                        child: buildProfileImage(),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 20),
-                    child: createUsername(),
-                  ),
-                  // Container(
-                  //   height: 250,
-                  //   width: double.infinity,
-                  //   margin: EdgeInsets.only(bottom: 50),
-                  //   child: Image.asset('images/bg.jpg', fit: BoxFit.cover),
-                  // ),
-                  // Container(
-                  //   margin: EdgeInsets.only(top: 170, right: 70, left: 20),
-                  //   child: buildProfileImage(),
-                  // ),
+                  buildCoverImage(),
+                  Positioned(top: 170, left: 50, child: buildProfileImage()),
                 ],
               ),
+              Container(
+                margin: EdgeInsets.only(left: 20),
+                child: createUsername(),
+              ),
+              // Container(
+              //   height: 250,
+              //   width: double.infinity,
+              //   margin: EdgeInsets.only(bottom: 50),
+              //   child: Image.asset('images/bg.jpg', fit: BoxFit.cover),
+              // ),
+              // Container(
+              //   margin: EdgeInsets.only(top: 170, right: 70, left: 20),
+              //   child: buildProfileImage(),
+              // ),
             ],
           ),
-          Stack(
-            children: [
-              Container(
-                color: Theme.of(context).colorScheme.primary,
-                height: 300,
-                margin: EdgeInsets.only(bottom: 20),
-              ),
-              Column(
-                spacing: 7,
-                children: [
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    margin: EdgeInsets.only(left: 10, top: 10, bottom: 10),
-                    child: Text(
-                      "Quick functions",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  makeNavButton(Icons.meeting_room, "Meeting", context),
-                  makeNavButton(Icons.task, "Task", context),
-                  makeNavButton(Icons.pie_chart, "Finance statics", context),
-                  makeNavButton(
-                    Icons.add_to_home_screen_sharp,
-                    "Tools",
-                    context,
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Stack(
-            children: [
-              Container(
-                color: Theme.of(context).colorScheme.primary,
-                height: 120,
-                margin: EdgeInsets.only(bottom: 20),
-              ),
-              Column(
-                children: [
-                  makeTitleText("Interface"),
-                  Row(
-                    spacing: 7,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        child: Icon(Icons.sunny, size: 30),
-                      ),
-                      Text("Theme", style: TextStyle(fontSize: 20)),
-                      Expanded(
-                        flex: 1,
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Container(
-                            margin: EdgeInsets.only(right: 30),
-                            child: makeThemeButton(), // toggle theme
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Stack(
-            children: [
-              Container(
-                color: Theme.of(context).colorScheme.primary,
-                height: 200,
-                margin: EdgeInsets.only(bottom: 20),
-              ),
-              Column(
-                spacing: 7,
-                children: [
-                  makeTitleText("Settings"),
-                  makeNavButton(Icons.file_copy_sharp, "About us", context),
-                  makeNavButton(Icons.help_center, "Help", context),
-                ],
-              ),
-            ],
-          ),
-          Stack(
-            children: [
-              Container(
-                color: Theme.of(context).colorScheme.primary,
-                height: 1000,
-                margin: EdgeInsets.only(bottom: 20),
-              ),
+          makeSpacer(context),
 
-              Column(
+          Column(
+            spacing: 7,
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                child: Text(
+                  "Quick functions",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+              ),
+              makeNavButton(Icons.meeting_room, "Meeting", context),
+              makeNavButton(Icons.task, "Task", context),
+              makeNavButton(Icons.pie_chart, "Finance statics", context),
+              makeNavButton(Icons.add_to_home_screen_sharp, "Tools", context),
+            ],
+          ),
+          makeSpacer(context),
+
+          Column(
+            children: [
+              makeTitleText("Interface"),
+              Row(
+                spacing: 7,
                 children: [
-                  makeTitleText("Account"),
-                  ListTile(
-                    title: Text(
-                      "Log out",
-                      style: TextStyle(color: Color(0xffFE2B25), fontSize: 30),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Icon(Icons.sunny, size: 30),
+                  ),
+                  Text("Theme", style: TextStyle(fontSize: 20)),
+                  Expanded(
+                    flex: 1,
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        margin: EdgeInsets.only(right: 30),
+                        child: makeThemeButton(), // toggle theme
+                      ),
                     ),
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            content: Text("Are you sure you want to log out?"),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop;
-                                },
-                                child: Text("Log out"),
-                              ),
-                            ],
-                          );
-                        },
+                  ),
+                ],
+              ),
+            ],
+          ),
+          makeSpacer(context),
+
+          Column(
+            spacing: 7,
+            children: [
+              makeTitleText("Settings"),
+              makeNavButton(Icons.file_copy_sharp, "About us", context),
+              makeNavButton(Icons.help_center, "Help", context),
+            ],
+          ),
+          makeSpacer(context),
+
+          Column(
+            children: [
+              makeTitleText("Account"),
+              ListTile(
+                title: Text(
+                  "Log out",
+                  style: TextStyle(color: Color(0xffFE2B25), fontSize: 30),
+                ),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: Text("Are you sure you want to log out?"),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop;
+                            },
+                            child: Text("Log out"),
+                          ),
+                        ],
                       );
                     },
-                  ),
-
-                  // Spacer(),
-                ],
+                  );
+                },
               ),
+
+              // Spacer(),
             ],
           ),
+
+          Container(height: 500),
+
           Align(
             child: Container(
               padding: EdgeInsets.all(7),
@@ -202,6 +155,12 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  Container makeSpacer(BuildContext context) => Container(
+    color: Theme.of(context).colorScheme.surface,
+    height: 30,
+    margin: EdgeInsets.only(top: 10, bottom: 10),
+  );
 
   Widget makeTitleText(String title) => Container(
     alignment: Alignment.centerLeft,
@@ -254,11 +213,14 @@ class _HomeScreenState extends State<HomeScreen> {
     ],
   );
 
-  Widget buildCoverImage() => Container(
-    height: 250,
-    width: double.infinity,
-    margin: EdgeInsets.only(bottom: 110),
-    child: Image.asset('images/bg.jpg', fit: BoxFit.cover),
+  Widget buildCoverImage() => Align(
+    alignment: Alignment.topCenter,
+    child: Container(
+      height: 250,
+      width: double.infinity,
+      margin: EdgeInsets.only(bottom: 110),
+      child: Image.asset('images/bg.jpg', fit: BoxFit.cover),
+    ),
   );
 
   Widget createUsername() => Row(
